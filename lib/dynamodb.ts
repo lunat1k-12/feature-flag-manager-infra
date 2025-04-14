90fa9bbb7ead48d85543aa3b67113b11ae4425dc
yvgenchemeris <chemeris.lunat1k@gmail.com>
2025-04-13 22:07:49 -0700

    update db definition

--- lib/dynamodb.ts
+++ lib/dynamodb.ts
@@ -1,7 +1,8 @@
 import * as cdk from "aws-cdk-lib";
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import {Construct} from "constructs";
 import {RemovalPolicy} from "aws-cdk-lib";
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import {ProjectionType} from 'aws-cdk-lib/aws-dynamodb';
+import {Construct} from "constructs";
 
 export class DynamoDbStack extends cdk.Stack {
 
@@ -17,8 +18,8 @@ export class DynamoDbStack extends cdk.Stack {
 
         const apiTable = new dynamodb.Table(this, 'ApiKeyTable', {
             tableName: 'EnvApiKey',
-            partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
-            sortKey: {name: 'EnvName', type: dynamodb.AttributeType.STRING },
+            partitionKey: { name: 'EnvName', type: dynamodb.AttributeType.STRING },
+            sortKey: {name: 'key', type: dynamodb.AttributeType.STRING },
             billingMode: dynamodb.BillingMode.PAY_PER_REQUEST, // On-demand pricing
             removalPolicy: RemovalPolicy.DESTROY, // Auto-delete table on stack removal
         });
